2464d4a1db0862c24cb35c2b86ddd95bdd23ac3e
nixnix233 <129942847+nixnix233@users.noreply.github.com>
2023-07-30 02:01:43 +0800

    feat: add disjoint set union

---
+++ DSU.cpp
@@ -0,0 +1,34 @@
+struct DSU {
+    std::vector<int> id, sz;
+    DSU() {}
+    DSU(int n) {
+        init(n);
+	}
+    void init(int n) {
+        id.resize(n);
+        std::iota(id.begin(), id.end(), 0);
+        sz.assign(n, 1);
+    }
+    int find(int x) {
+		if (id[x] != x) {
+			id[x] = find(id[x]);
+		}
+		return id[x];
+    }
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+    bool merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
+            return false;
+        }
+        sz[x] += sz[y];
+        id[y] = x;
+        return true;
+    }
+    int size(int x) {
+        return sz[find(x)];
+    }
+};
