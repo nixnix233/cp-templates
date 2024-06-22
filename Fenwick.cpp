3f8f360b8ff4a0150f7972f119c6d2ace3218a6a
nixnix233 <129942847+nixnix233@users.noreply.github.com>
2024-06-22 19:42:46 +0800

    feat: Fenwick tree is 0-indexed now

--- Fenwick.cpp
+++ Fenwick.cpp
@@ -2,22 +2,23 @@ template <typename T>
 struct Fenwick {
     int n;
     std::vector<T> a;
-    Fenwick(int n) : n{1 + n} {
-        a.assign(1 + n, T{});
+    Fenwick(int n) : n{n} {
+        a.assign(n, T{});
     }
     void add(int k, int x) {
+        k++;
         for (int i = k; i <= n; i += i & -i) {
-            a[i] += x;
+            a[i - 1] += x;
         }
     }
     T sum(int k) {
         T ans{};
         for (int i = k; i > 0; i -= i & -i) {
-            ans += a[i];
+            ans += a[i - 1];
         }
         return ans;
     }
     T sum(int l, int r) {
-        return sum(r) - sum(l - 1);
+        return sum(r) - sum(l);
     }
 };
